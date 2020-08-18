0ed2e6c2c1898b5f04ab025f9656804aa28f2de7
Arilik <sasha-chagdurov@mail.ru>
2020-08-18 22:09:41 +0800

    ending build webpack

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import *as $ from 'jquery'
-import Post from './Post'
-import './styles/style.css'
-import './styles/less.less'
-const post = new Post ('Webpack Post Title')
-$(pre).html(post.toString())
--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,40 @@ const { LoaderOptionsPlugin } = require("webpack");
 const { ALL } = require("dns");
 const { extname } = require("path");
 const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+const babelOptions = preset => {
+  const opts = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+
+  if (preset) {
+    opts.presets.push(preset)
+  }
+
+  return opts
+}
+const cssLoaders = extra => {
+      const loaders = [{
+        loader: MiniCSSExtractPlugin.loader,
+        options: {
+          hmr: isDev,
+        },
+      }, 'css-loader']
+      if (extra) {
+        loaders.push(extra)
+      }
+      return loaders
+}
 module.exports = {
     context: path.resolve(__dirname, 'src'),
   mode: "development",
   entry: {
-    main: "./index.js", 
+    main: ['@babel/polyfill','./index.jsx'], 
+    analytics: './analytics.ts'
   }, 
   output: {
     filename: filename('js'),
@@ -23,6 +52,11 @@ module.exports = {
         chunks: 'all'
     }
   },
+  devServer: {
+    port: 4200,
+    hot: isDev
+  },
+  devtool: isDev ? 'source-map' : '',
   plugins: [
       new HTMLWebpackPlugin({
           template: './index.html',
@@ -39,26 +73,16 @@ module.exports = {
       rules: [
           {
               test: /\.css$/,
-              use: [{
-                loader: MiniCSSExtractPlugin.loader,
-                options: {
-                  publicPath: '../',
-                  hmr: isDev,
-                },
-              }, 'css-loader']
+              use: cssLoaders()
           },
           {
             test: /\.less$/,
-            use: [{
-              loader: MiniCSSExtractPlugin.loader,
-              options: {
-                publicPath: '../',
-                hmr: isDev,
-              },
-            }, 'css-loader',
-            'less-loader'
-          ]
+            use: cssLoaders('less-loader')
         },
+        {
+          test: /\.s[ac]ss$/,
+          use: cssLoaders('sass-loader')
+      },
           {
             test: /\.(png|jpg|svg|gif)$/,
             use: ['file-loader']
@@ -66,6 +90,27 @@ module.exports = {
           {
               test: /\.(ttf|wof|wof2|)$/,
               use: ['file-loader']
+          },
+          { test: /\.js$/, 
+            exclude: /node_modules/, 
+            loader: {
+              loader: 'babel-loader',
+              options: babelOptions()
+            } 
+          },
+          { test: /\.ts$/, 
+            exclude: /node_modules/, 
+            loader: {
+              loader: 'babel-loader',
+              options: babelOptions('@babel/preset-typescript')
+            } 
+          },
+          { test: /\.jsx$/, 
+            exclude: /node_modules/, 
+            loader: {
+              loader: 'babel-loader',
+              options: babelOptions('@babel/preset-react')
+            } 
           }
       ]
   }
