44b91c5290000646e1452d8d559e598629e8930c
Arilik <sasha-chagdurov@mail.ru>
2020-08-18 16:19:05 +0800

    add new plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import *as $ from 'jquery'
 import Post from './Post'
 import './styles/style.css'
+import './styles/less.less'
 const post = new Post ('Webpack Post Title')
 $(pre).html(post.toString())
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require("path");
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
 const { LoaderOptionsPlugin } = require("webpack");
+const { ALL } = require("dns");
+const { extname } = require("path");
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
 module.exports = {
     context: path.resolve(__dirname, 'src'),
   mode: "development",
@@ -9,24 +15,53 @@ module.exports = {
     main: "./index.js", 
   }, 
   output: {
-    filename: "[name].[contenthash].js",
+    filename: filename('js'),
     path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    splitChunks: {
+        chunks: 'all'
+    }
   },
   plugins: [
       new HTMLWebpackPlugin({
-          template: './index.html'
+          template: './index.html',
+          minify: {
+            collapseWhitespace: isProd
+          }
       }),
-      new CleanWebpackPlugin()
+      new CleanWebpackPlugin(),
+      new MiniCSSExtractPlugin({
+          filename: filename('css'),
+      })
   ],
   module: {
       rules: [
           {
               test: /\.css$/,
-              use: ['style-loader', 'css-loader']
+              use: [{
+                loader: MiniCSSExtractPlugin.loader,
+                options: {
+                  publicPath: '../',
+                  hmr: isDev,
+                },
+              }, 'css-loader']
           },
           {
-              test: /\.(png|jpg|svg|gif)$/,
-              use: ['file-loader']
+            test: /\.less$/,
+            use: [{
+              loader: MiniCSSExtractPlugin.loader,
+              options: {
+                publicPath: '../',
+                hmr: isDev,
+              },
+            }, 'css-loader',
+            'less-loader'
+          ]
+        },
+          {
+            test: /\.(png|jpg|svg|gif)$/,
+            use: ['file-loader']
           },
           {
               test: /\.(ttf|wof|wof2|)$/,
